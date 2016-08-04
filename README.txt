bc28b82bb36e8659e6efd56c5d72371439943823
Emran Ahmed <emran.bd.08@gmail.com>
2016-08-04 19:28:44 +0600

    Init

---
+++ README.txt
@@ -0,0 +1,61 @@
+=== Woo Cart Redirect To Checkout Page ===
+Contributors: EmranAhmed
+Tags: woocommerce, checkout, cart, redirect
+Requires at least: 4.3
+Tested up to: 4.5.3
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+After successfully product added to cart redirect to checkout page.
+
+== Description ==
+The **Woo Cart Redirect To Checkout Page** plugin for WooCommerce enables you to redirect directly checkout page rather than redirect to cart page.
+
+= Links =
+* [Github](https://github.com/EmranAhmed/woo-cart-redirect-to-checkout-page/?utm_medium=referral&utm_source=wordpress.org&utm_campaign=Woo+AWS+S3+Readme&utm_content=Repo+Link)
+
+== Installation ==
+
+### Automatic Install From WordPress Dashboard
+
+1. Login to your the admin panel
+2. Navigate to Plugins -> Add New
+3. Search **Woo Cart Redirect To Checkout Page**
+4. Click install and activate respectively.
+
+### Manual Install From WordPress Dashboard
+
+If your server is not connected to the Internet, then you can use this method-
+
+1. Download the plugin by clicking on the red button above. A ZIP file will be downloaded.
+2. Login to your site's admin panel and navigate to Plugins -> Add New -> Upload.
+3. Click choose file, select the plugin file and click install
+
+### Install Using FTP
+
+If you are unable to use any of the methods due to internet connectivity and file permission issues, then you can use this method-
+
+1. Download the plugin by clicking on the red button above. A ZIP file will be downloaded.
+2. Unzip the file.
+3. Launch your favorite FTP client. Such as FileZilla, FireFTP, CyberDuck etc. If you are a more advanced user, then you can use SSH too.
+4. Upload the folder to wp-content/plugins/
+5. Log in to your WordPress dashboard.
+6. Navigate to Plugins -> Installed
+7. Activate the plugin
+
+== Screenshots ==
+
+1. Settings
+
+== Frequently Asked Questions ==
+
+= How to enable checkout page redirect =
+
+- Goto **WooCommerce -> Settings -> Product tab -> Display** and Check  *Redirect to the checkout page after successful addition*.
+
+== Changelog ==
+
+= 1.0.0 =
+
+Initial release
---
+++ woo-cart-redirect-to-checkout-page.php
@@ -0,0 +1,115 @@
+<?php
+	/**
+	 * Plugin Name:  Woo Cart Redirect To Checkout Page
+	 * Plugin URI:   https://wordpress.org/plugins/woo-cart-redirect-to-checkout-page/
+	 * Description:  Redirect to checkout page after successfully product added to cart.
+	 * Version:      1.0.0
+	 * Author:       Emran
+	 * Author URI:   https://emran.me/
+	 * License:      GPLv2.0+
+	 * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+	 *
+	 * Text Domain:  woo-cart-redirect-to-checkout-page
+	 * Domain Path:  /languages/
+	 */
+
+	defined( 'ABSPATH' ) or die( 'Keep Quit' );
+
+	if ( ! class_exists( 'Woo_Cart_Redirect_To_Checkout_Page' ) ):
+
+		class Woo_Cart_Redirect_To_Checkout_Page {
+
+			public function __construct() {
+				$this->constants();
+				$this->hooks();
+				do_action( 'woo_cart_redirect_to_checkout_page_loaded', $this );
+			}
+
+			public function constants() {
+				define( 'WOO_CRTCP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+				define( 'WOO_CRTCP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+				define( 'WOO_CRTCP_PLUGIN_DIRNAME', dirname( plugin_basename( __FILE__ ) ) );
+				define( 'WOO_CRTCP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+				define( 'WOO_CRTCP_PLUGIN_FILE', __FILE__ );
+			}
+
+			public function hooks() {
+
+				// Init
+				add_action( 'init', array( $this, 'init' ), 0 );
+
+				// Product Settings Option
+				add_filter( 'woocommerce_product_settings', array( $this, 'settings' ) );
+
+				// Cart Redirect
+				add_filter( 'woocommerce_add_to_cart_redirect', array( $this, 'cart_redirect' ) );
+
+				// Ajax Add to cart redirect
+				add_filter( 'wc_add_to_cart_params', array( $this, 'add_to_cart_js_params' ) );
+
+				// Plugin Row Meta
+				add_filter( 'plugin_action_links_' . WOO_CRTCP_PLUGIN_BASENAME, array( $this, 'plugin_action_links' ) );
+			}
+
+			public function init() {
+				// Before init action.
+				do_action( 'before_woo_cart_redirect_to_checkout_page_init' );
+				load_plugin_textdomain( 'woo-cart-redirect-to-checkout-page', FALSE, WOO_CRTCP_PLUGIN_DIRNAME . '/languages' );
+				do_action( 'woo_cart_redirect_to_checkout_page_init' );
+			}
+
+			public function add_to_cart_js_params( $data ) {
+
+				if ( 'yes' === get_option( 'ea_woo_cart_redirect_checkout', 'yes' ) ) {
+					$data[ 'cart_redirect_after_add' ] = get_option( 'ea_woo_cart_redirect_checkout', 'yes' );
+				}
+
+				return $data;
+			}
+
+			public function cart_redirect( $url ) {
+				if ( 'yes' === get_option( 'ea_woo_cart_redirect_checkout', 'yes' ) ) {
+					return wc_get_checkout_url();
+				}
+
+				return $url;
+			}
+
+			public function plugin_action_links( $links ) {
+
+				if ( is_plugin_active( WOO_CRTCP_PLUGIN_BASENAME ) ) {
+					$action_links = apply_filters( 'woo_cart_redirect_to_checkout_page_action_links', array(
+						'settings' => sprintf( '<a href="%1$s" title="%2$s">%2$s</a>', esc_url( add_query_arg( array(
+							                                                                                       'page'    => 'wc-settings',
+							                                                                                       'tab'     => 'products',
+							                                                                                       'section' => 'display',
+						                                                                                       ), admin_url( 'admin.php' ) ) ), esc_attr__( 'Settings', 'woo-cart-redirect-to-checkout-page' ) ),
+					) );
+
+					return array_merge( $action_links, $links );
+				}
+
+				return (array) $links;
+			}
+
+			public function settings( $settings ) {
+
+				array_splice( $settings, 6, 0, array(
+					array(
+						'desc'          => esc_html__( 'Redirect to the checkout page after successful addition.', 'woo-cart-redirect-to-checkout-page' ),
+						'id'            => 'ea_woo_cart_redirect_checkout',
+						'default'       => 'yes',
+						'type'          => 'checkbox',
+						'checkboxgroup' => '',
+						'desc_tip'      => __( 'This setting will overwrite "Redirect to the cart page after successful addition" settings. <br> Means it will redirect to checkout page rather than cart page after successful addition.', 'woo-cart-redirect-to-checkout-page' ),
+					)
+				) );
+
+				return $settings;
+
+			}
+		}
+
+		new Woo_Cart_Redirect_To_Checkout_Page();
+
+	endif;
